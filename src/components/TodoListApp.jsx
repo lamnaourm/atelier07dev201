180e879b07235e64035ed4797e4a4a74aeaa9ca7
lamnaourm <mohammed.lamnaour@gmail.com>
2023-10-26 09:28:54 +0100

    Ajout Tache

--- src/components/TodoListApp.jsx
+++ src/components/TodoListApp.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { AiOutlineCheck } from 'react-icons/ai'
 import { ImCancelCircle } from 'react-icons/im'
+import {GrAdd} from 'react-icons/gr'
 
 export default function TodoListApp() {
 
@@ -9,9 +10,30 @@ export default function TodoListApp() {
         { description: 'Réviser mongodb', completed: false },
         { description: 'Réviser javascript', completed: true }
     ])
+    const [task, setTask] = useState('')
 
+    const addTache = () => {
+        if(task.trim().length===0) {
+            alert('Description vide')
+            return;
+        }
+
+        if(taches.filter(t => t.description.toLowerCase()===task.toLowerCase()).length>0){
+            alert('Tache existe deja')
+            return;
+        }
+
+        setTaches(taches => [...taches, {description:task, completed:false}])
+        setTask('')
+        document.getElementById('task').focus();
+    }
     return (
         <div className='app'>
+            <fieldset>
+                <legend>Ajout</legend>
+                <input type="text" name="task" id="task" value={task} onChange={(e) => setTask(e.target.value)} />
+                <button onClick={addTache}><GrAdd/> Add</button>
+            </fieldset>
             <div className='list'>
                 {taches.map(t =>
                     <div className='tache'>
