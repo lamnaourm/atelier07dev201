d6f36dd8227f7521f23da039c6b0a3642b3f1a45
lamnaourm <mohammed.lamnaour@gmail.com>
2023-10-26 09:06:47 +0100

    styles

--- src/components/TodoListApp.jsx
+++ src/components/TodoListApp.jsx
@@ -1,25 +1,27 @@
 import React, { useState } from 'react'
-import {AiOutlineCheck} from 'react-icons/ai'
-import {ImCancelCircle} from 'react-icons/im'
+import { AiOutlineCheck } from 'react-icons/ai'
+import { ImCancelCircle } from 'react-icons/im'
 
 export default function TodoListApp() {
 
     const [taches, setTaches] = useState([
-        {description:'Réviser HTML/CSS', completed:true},
-        {description:'Réviser mongodb', completed:false},
-        {description:'Réviser javascript', completed:true}
+        { description: 'Réviser HTML/CSS', completed: true },
+        { description: 'Réviser mongodb', completed: false },
+        { description: 'Réviser javascript', completed: true }
     ])
 
-  return (
-    <div>
-        {taches.map(t =>
-            <div className='tache'>
-                <h4>{t.description}</h4>
-                {
-                    t.completed ? <AiOutlineCheck /> : <ImCancelCircle />
-                }
+    return (
+        <div className='app'>
+            <div className='list'>
+                {taches.map(t =>
+                    <div className='tache'>
+                        <h4>{t.description}</h4>
+                        {
+                            t.completed ? <AiOutlineCheck /> : <ImCancelCircle />
+                        }
+                    </div>
+                )}
             </div>
-        )}
-    </div>
-  )
+        </div>
+    )
 }
